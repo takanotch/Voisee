fbf5e4b90523fbf3d0ffb5df0cc1d58903b90192
takanotch <76425423+takanotch@users.noreply.github.com>
2023-07-13 00:13:00 +0900

    Fix bug

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import time
 import numpy as np
 import glob as glob
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from keras.utils import np_utils
 import librosa as lb
 import librosa.display
